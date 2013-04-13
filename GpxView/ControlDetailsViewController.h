5c680845aa27444c1334ad1a89edadf896f48d62
Mario Martelli <cocoa@schnuddelhuddel.de>
2013-04-13 21:41:23 +0200

    Added tableView for controls

---
+++ GpxView/ControlDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ControlDetailsViewController.h
+//  Audax
+//
+//  Created by Mario Martelli on 08.04.13.
+//  Copyright (c) 2013 Schnuddel Huddel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ControlDetailsViewController : UIViewController
+
+@end
---
+++ GpxView/ControlDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ControlDetailsViewController.m
+//  Audax
+//
+//  Created by Mario Martelli on 08.04.13.
+//  Copyright (c) 2013 Schnuddel Huddel. All rights reserved.
+//
+
+#import "ControlDetailsViewController.h"
+
+@interface ControlDetailsViewController ()
+
+@end
+
+@implementation ControlDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
